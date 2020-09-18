85ac65ea6654828ab2dbe0b6f5cad4b91fc932f8
Пэйнт <xcession@gmail.com>
2020-09-18 17:08:30 +0700

    Working navigation

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
 import React from "react";
 import * as eva from "@eva-design/eva";
-import {
-  ApplicationProvider,
-  Button,
-  Layout,
-  Text,
-} from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import { AppNavigator } from "./navigation.component";
 
-const App = () => {
-  return (
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      >
-        <Text category='h1'>HELLO</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button
-          icon='camera'
-          mode='contained'
-          onPress={() => console.log("Pressed")}
-        >
-          Press me
-        </Button>
-      </Layout>
+      <AppNavigator />
     </ApplicationProvider>
-  );
-};
-
-export default App;
+  </>
+);
