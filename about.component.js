948b747c2ee726e3d5ec20b451ba134f78b8ead2
Пэйнт <xcession@gmail.com>
2020-09-20 23:08:35 +0700

    Finish about page

--- about.component.js
+++ about.component.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import {
+  Image,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 import {
   Divider,
   Icon,
@@ -8,6 +15,7 @@ import {
   TopNavigation,
   TopNavigationAction,
 } from "@ui-kitten/components";
+import { version } from "./app.json";
 
 const BackIcon = (props) => <Icon {...props} name='arrow-back' />;
 
@@ -28,8 +36,31 @@ export const AboutScreen = ({ navigation }) => {
         accessoryLeft={BackAction}
       />
       <Divider />
-      <Layout style={styles.container}>
-        <Text category='h1'>ABOUT</Text>
+      <Layout
+        style={[
+          styles.container,
+          { justifyContent: "center", alignItems: "center" },
+        ]}
+      >
+        <Icon
+          style={{ width: 128, height: 128 }}
+          fill='#7B847A'
+          name='github-outline'
+        />
+        <Text style={styles.fontPrimary} category='h1'>
+          Git Reference
+        </Text>
+        <Text
+          style={[styles.textVersion, { marginBottom: 8 }]}
+          category='c1'
+          appearance='hint'
+        >
+          Version {Constants.manifest.version}
+        </Text>
+        <Image
+          style={{ width: 136, height: 20 }}
+          source={require("./assets/logo-rev.png")}
+        />
       </Layout>
     </SafeAreaView>
   );
@@ -42,4 +73,7 @@ const styles = StyleSheet.create({
   droidSafeArea: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  fontPrimary: { fontFamily: "RobotoSlab_700Bold" },
+  fontSecondary: { fontFamily: "Roboto_400Regular" },
+  textVersion: { fontFamily: "RobotoMono_400Regular" },
 });
